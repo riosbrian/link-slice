d564f2bde99524c6635297c2c35eff89967e8739
Brian Rios <riosbrian.ar@gmail.com>
2024-04-09 23:18:39 +0200

    .env server variables changed

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,16 +1,17 @@
+import config from "../config/config.js";
 import * as AuthService from "../services/auth.service.js";
 import { generateAccessToken } from "../utils/jwt.js";
-const CLIENT_URL = "http://localhost:5173";
+const { CLIENT_URL } = config;
 
 export const loginWithGithub = async (req, res, next) => {
   const user = req.user;
   try {
     const token = generateAccessToken(user);
     const options = {
-      maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+      maxAge: 24 * 60 * 60 * 1000,
       httpOnly: true,
-      // secure: true, // Solo enviar en conexiones HTTPS
-      // sameSite: "None", // Permitir cookies de terceros en solicitudes entre sitios
+      secure: true,
+      sameSite: "None",
     };
     res.cookie("accessToken", token, options);
     res.redirect(CLIENT_URL);
@@ -25,10 +26,10 @@ export const loginWithGoogle = async (req, res, next) => {
   try {
     const token = generateAccessToken(user);
     const options = {
-      maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+      maxAge: 24 * 60 * 60 * 1000,
       httpOnly: true,
-      // secure: true, // Solo enviar en conexiones HTTPS
-      // sameSite: "None", // Permitir cookies de terceros en solicitudes entre sitios
+      secure: true,
+      sameSite: "None",
     };
     res.cookie("accessToken", token, options);
     res.redirect(CLIENT_URL);
